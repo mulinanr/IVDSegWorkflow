96b231804264629bd639d917b1aafc8d21135806
Anri Mulin <anri.mulin@uni-duesseldorf.de>
2019-03-29 23:39:23 +0100

    str concatenating error fixed

--- dicomLoader.py
+++ dicomLoader.py
@@ -12,7 +12,7 @@ PathResult = "../DICOM_RESULT/"
 def extract_series(ds):
     SeriesDescription = re.sub(':', '', ds.SeriesDescription)
     SeriesInstanceUID = ds.SeriesInstanceUID.split('.0.0', 1)[0][-5:]
-    return SeriesDescription + '_' + SeriesInstanceUID
+    return str(SeriesDescription + '_' + SeriesInstanceUID)
 
 def create_directory_list(path):
     lstDirsDCM = []  
@@ -25,7 +25,7 @@ def rename_dicom_file(ds, oldFilename):
     AcquisitionNumber = 0
     if 'AcquisitionNumber' in ds:
         AcquisitionNumber = ds.AcquisitionNumber
-
+    
     newFilename = os.path.join(PathResult, 
                                extract_series(ds),
                                extract_series(ds) + '_sl_' + 
@@ -37,9 +37,14 @@ def copy_file(fileDCM):
     try:
         ds = pydicom.dcmread(fileDCM)
         rename_dicom_file(ds, fileDCM)
+    except pydicom.errors.InvalidDicomError:
+        print('The file ' + fileDCM + ' is not a DICOM file')
+        pass
+    except IsADirectoryError:
+        print('The file ' + fileDCM + ' is a directory')
+        pass
     except:
-        #print('The file ' + fileDCM + ' is not a DICOM file')
-        #print("Unexpected error:", sys.exc_info()[0])
+        print("Unexpected error:", sys.exc_info()[0])
         pass
 
 def sort_dicom_files(lstDirsDCM):
@@ -62,16 +67,20 @@ def create_series_set(lstDirsDCM):
     return seriesSet
 
 def prepare_destinations(seriesSet, path):
-    os.system('rm -f -r %s' % os.path.join(path))
-    os.mkdir(path)
+    #shutil.rmtree(os.path.join(path), ignore_errors=True)
+
+    if not os.path.isdir(os.path.join(path)):
+        os.mkdir(path)
+
     for serie in seriesSet:
-        os.system('rm -f -r %s' % os.path.join(path, serie))
-        os.system('mkdir  %s' % os.path.join(path, serie))
+        shutil.rmtree(os.path.join(path, serie), ignore_errors=True)
+        os.mkdir(os.path.join(path, serie))
 
 def load_dicom_files():
     print('Sort DICOM files')
     dirList = create_directory_list(PathDicom)
-    seriesSet = create_series_set(dirList)  
+    seriesSet = create_series_set(dirList)
+    #print(seriesSet)
     prepare_destinations(seriesSet, PathResult)
     sort_dicom_files(dirList)
     print('Done')
