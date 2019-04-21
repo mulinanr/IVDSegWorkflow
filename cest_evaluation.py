6b268cfff40d60a10e18804887deef7a9bb3723f
Anri Mulin <anri.mulin@uni-duesseldorf.de>
2019-04-21 20:06:56 +0200

    Image show functions added

--- cest_evaluation.py
+++ cest_evaluation.py
@@ -7,7 +7,6 @@ from PIL import Image, ImageTk
 from cest import cest_corrector
 from cest import mtr_asym_calculator
 from utils import common_functions
-from utils import draw_image
 from wassr import algorithm
 from wassr import mscf_algorithm
 from wassr import wassr_corrector
@@ -27,8 +26,10 @@ def processMri(wassrPath, cestPath, wassrProperties, cestProperties):
     #   3.1. Display if needed
     rootMask = tk.Tk()
     rootMask.title("Makse")
-    imgMask =  ImageTk.PhotoImage(image = Image.fromarray(common_functions.interval_mapping(Mask, 0.0, 1.0, 0, 255)), master = rootMask)
-    canvas = tk.Canvas(rootMask, width = 230, height = 230)
+    imageMask = Image.fromarray(common_functions.interval_mapping(Mask, 0.0, 1.0, 0, 255))
+    imageMask = imageMask.resize((576, 576))
+    imgMask =  ImageTk.PhotoImage(image = imageMask, master = rootMask)
+    canvas = tk.Canvas(rootMask, width = 620, height = 620)
     canvas.pack()
     canvas.create_image(20, 20, anchor = "nw", image = imgMask)
 
@@ -41,12 +42,15 @@ def processMri(wassrPath, cestPath, wassrProperties, cestProperties):
                 wassrProperties.hStep, wassrProperties.maxOffset, wassrProperties.alternating, wassrProperties.nDynamics, 
                 wassrProperties.lmo, wassrProperties.gauss, wassrProperties.zFilter, algoritm)
     #(Offsets, R) = wassrCorrector.calculateWassrAmlCorrection(wassrPath, sName, filename, Mask)
+ 
     #   4.1. Display if needed
     Offsets = np.ones((192, 192)) * 0.7
     rootWassr = tk.Tk()
     rootWassr.title("Offset")
-    imgWassr =  ImageTk.PhotoImage(image = Image.fromarray(common_functions.interval_mapping(Offsets, 0.0, 1.0, 0, 255)), master = rootWassr)
-    canvas = tk.Canvas(rootWassr, width = 230, height = 230)
+    imageWassr = Image.fromarray(common_functions.interval_mapping(Offsets, 0.0, 1.0, 0, 255))
+    imageWassr = imageWassr.resize((576, 576))
+    imgWassr =  ImageTk.PhotoImage(image = imageWassr, master = rootWassr)
+    canvas = tk.Canvas(rootWassr, width = 620, height = 620)
     canvas.pack()
     canvas.create_image(20, 20, anchor = "nw", image = imgWassr)
 
@@ -57,15 +61,27 @@ def processMri(wassrPath, cestPath, wassrProperties, cestProperties):
     cestCorrector = cest_corrector.CestCorrector(cestProperties.sSlide, cestProperties.hStep, cestProperties.maxOffset, 
                 cestProperties.abreite, cestProperties.fshift, cestProperties.dfreq, cestProperties.alternating, 
                 cestProperties.nDynamics, cestProperties.gauss, cestProperties.S0yn, cestProperties.zFilter)
-    (CestCurveS, x_calcentries) = cestCorrector.calculateCestAmlEvaluation(cestPath, OF, sName, filename, Mask)
-    #   5.1. Dispalay if needed
-    #   5.2. Save if
+    #(CestCurveS, x_calcentries) = cestCorrector.calculateCestAmlEvaluation(cestPath, OF, sName, filename, Mask)
+ 
+    #   5.1. Save to SName if needed
 
 
     # 6. Calculate MTR Async
     mtrAsymCalculator = mtr_asym_calculator.MtrAsymCalculator(cestProperties.fshift, cestProperties.dfreq)
-    mtrAsymCalculator.calculateMtrAsymCurves(CestCurveS, x_calcentries, Mask)
+    #(MTRasymCurves, MTRasym_Bild) = mtrAsymCalculator.calculateMtrAsymCurves(CestCurveS, x_calcentries, Mask)
+
     #   6.1. Display if needed
+    MTRasym_Bild = np.ones((192, 192)) * 0.7
+    rootMtr = tk.Tk()
+    rootMtr.title("MTRasym")
+    imageMtr = Image.fromarray(common_functions.interval_mapping(MTRasym_Bild, 0.0, 1.0, 0, 255))
+    imageMtr = imageMtr.resize((576, 576))
+    imgMtr =  ImageTk.PhotoImage(image = imageMtr, master = rootMtr)
+    canvas = tk.Canvas(rootMtr, width = 620, height = 620)
+    canvas.pack()
+    canvas.create_image(20, 20, anchor = "nw", image = imgMtr)
+    rootMtr.mainloop()
+
     #   6.2. Save if needed
 
     pass
