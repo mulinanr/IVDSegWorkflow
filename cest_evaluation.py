9a464d496e1649918a436d718f2565321898fe99
Anri Mulin <anri.mulin@uni-duesseldorf.de>
2019-04-21 17:05:37 +0200

    Drawing Images from Array added

--- cest_evaluation.py
+++ cest_evaluation.py
@@ -1,8 +1,13 @@
 import os
 
+import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk
+
 from cest import cest_corrector
 from cest import mtr_asym_calculator
 from utils import common_functions
+from utils import draw_image
 from wassr import algorithm
 from wassr import mscf_algorithm
 from wassr import wassr_corrector
@@ -19,14 +24,32 @@ def processMri(wassrPath, cestPath, wassrProperties, cestProperties):
     # 3. Get Mask, temporary use test Mask
     Mask = common_functions.createTestMask(192, 192, 5)
 
+    #   3.1. Display if needed
+    rootMask = tk.Tk()
+    rootMask.title("Makse")
+    imgMask =  ImageTk.PhotoImage(image = Image.fromarray(common_functions.interval_mapping(Mask, 0.0, 1.0, 0, 255)), master = rootMask)
+    canvas = tk.Canvas(rootMask, width = 230, height = 230)
+    canvas.pack()
+    canvas.create_image(20, 20, anchor = "nw", image = imgMask)
+
+    #   3.2. Save to SName if needed
+
 
     # 4. Make WASSR calculation
     algoritm = mscf_algorithm.MscfAlgorithm(wassrProperties.hStep, wassrProperties.maxOffset, wassrProperties.maxOffset)
     wassrCorrector = wassr_corrector.WassrCorrector(wassrProperties.sSlide, 
                 wassrProperties.hStep, wassrProperties.maxOffset, wassrProperties.alternating, wassrProperties.nDynamics, 
                 wassrProperties.lmo, wassrProperties.gauss, wassrProperties.zFilter, algoritm)
-    (OF, R) = wassrCorrector.calculateWassrAmlCorrection(wassrPath, sName, filename, Mask)
+    #(Offsets, R) = wassrCorrector.calculateWassrAmlCorrection(wassrPath, sName, filename, Mask)
     #   4.1. Display if needed
+    Offsets = np.ones((192, 192)) * 0.7
+    rootWassr = tk.Tk()
+    rootWassr.title("Offset")
+    imgWassr =  ImageTk.PhotoImage(image = Image.fromarray(common_functions.interval_mapping(Offsets, 0.0, 1.0, 0, 255)), master = rootWassr)
+    canvas = tk.Canvas(rootWassr, width = 230, height = 230)
+    canvas.pack()
+    canvas.create_image(20, 20, anchor = "nw", image = imgWassr)
+
     #   4.2. Save to SName if needed
 
 
--- utils/common_functions.py
+++ utils/common_functions.py
@@ -13,7 +13,12 @@ def str2bool(value):
         return True
     else:
         return False
-    
+ 
+def interval_mapping(image, from_min, from_max, to_min, to_max):
+    from_range = from_max - from_min
+    to_range = to_max - to_min
+    scaled = np.array((image - from_min) / float(from_range), dtype=float)
+    return to_min + (scaled * to_range)   
 
 def defineSName(imageDirectory, sSlide):
     pathArray = os.path.normpath(imageDirectory).lstrip(os.path.sep).split(os.path.sep)
